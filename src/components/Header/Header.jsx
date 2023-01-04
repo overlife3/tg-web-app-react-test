073a802473fe029b96eddcced0576ec929241273
overlife3 <102425953+overlife3@users.noreply.github.com>
2023-01-04 21:35:31 +0300

    5

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ export function Header() {
 		<div className={"header"}>
 			<Button onClick={onClose}>Закрыть</Button>
 			<span className={"username"}>
-				{user?.username}
+				{user?.username + " топ"}
 			</span>
 		</div>
 	)
