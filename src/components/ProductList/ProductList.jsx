3e1b0f0282c5d67d4385a3b4a7d9d2d0d6ca678c
overlife3 <102425953+overlife3@users.noreply.github.com>
2023-01-07 13:43:32 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./ProductList.css"
+import ProductItem from '../ProductItem/ProductItem'
+import { useTelegram } from '../hooks/useTelegram'
+
+const products = [
+	{id: '1', title: "Джинсы", price: 5000, description: "Синего цвета, прямые"},
+	{id: '2', title: "Куртка", price: 15000, description: "Зеленого цвета, теплая"},
+	{id: '3', title: "Джинсы 2", price: 2000, description: "Красного цвета, прямые"},
+	{id: '4', title: "КУртка 2", price: 3000, description: "Розового цвета, теплая"},
+	{id: '5', title: "Джинсы 3", price: 2000, description: "Серого цвета, прямые"},
+	{id: '6', title: "Куртка 3", price: 15000, description: "Белого цвета, теплая"},
+	{id: '7', title: "Джинсы 4", price: 12000, description: "Черного цвета, прямые"},
+	{id: '8', title: "Куртка 4", price: 13000, description: "Голубого цвета, теплая"},
+]
+
+const getTotalPrice = (items = []) => {
+	return items.reduce((acc, item) => {
+		return acc += item.price
+	}, 0)
+}
+
 export default function ProductList() {
+	const [addedItems, setAddedItems] = useState([])
+	const {tg} = useTelegram()
+	const onAdd = (product) => {
+		const alreadyAdded = addedItems.find(item => item.id === product.id);
+		let newItems = []
+		if (alreadyAdded) {
+			newItems = addedItems.filter(item => item.id !== product.id)
+		} else {
+			newItems = [...addedItems, product]
+		}
+
+		setAddedItems(newItems)
+
+		if(newItems.length === 0) {
+			tg.MainButton.hide()
+		} else {
+			tg.MainButton.show()
+			tg.MainButton.setParams({
+				text: `Купить ${getTotalPrice(newItems)}`
+			})
+		}
+	}
 	return (
-		<div>ProductList</div>
+		<div className='list'>
+			{products.map(item => (
+				<ProductItem 
+					product={item}
+					onAdd={onAdd}
+					className={"item"}				
+				/>
+			))}
+		</div>
 	)
 }
